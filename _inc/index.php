3ddecdc23592f31ec08564c484ed2ee5f06cdc6e
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2024-01-12 11:14:03 +0100

    modif

--- _inc/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-// instancier autoloader
-require '_inc/Classes/Autoloader/autoloader.php';
-
-Autoloader::register();
-
-// la désormais on peut appeler les classes sans avoir a faire de require
