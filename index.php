01568415c1aea40fb4b32f4a1363483724283e41
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2024-01-12 11:49:12 +0100

    ajout de la navbarre plus mise en placde de l'index

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kil's quizz</title>
+    <link rel="stylesheet" href="Assets/css/base.css"> <!-- Ajoutez cette ligne pour inclure le fichier CSS -->
+</head>
+<body>
+
+
 <?php
 // SPL autoloader
 require 'Classes/Autoloader/autoloader.php'; 
 Autoloader::register(); 
 
+require 'templates/navBar.php';
+
+echo "<p>test</p>";
+
+
+
+?>
+
+</body>
+</html>
